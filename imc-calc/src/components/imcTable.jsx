7ff4631c25e360e1f85eeb9feee4639158497bc6
Lucas Henrique <lucashenriquedesouza2@gmail.com>
2025-12-23 02:38:38 +0000

    Primeiro commit: calculadora IMC

---
+++ imc-calc/src/components/imcTable.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import Button from './Button'
+
+import './imcTable.css'
+import ImcCalc from './ImcCalc'
+
+const ImcTable = ({ data, imc, info, infoClass, backToBegin}) => {
+  
+  return (
+    <div id='result-container'>
+      <p id="imc-number">
+        Seu imc: <span className={infoClass}>{imc}</span>
+      </p>
+      <p id="imc-info">Situação atual: <span className={infoClass}>{info}</span></p>
+      <h3>Confira as classificações</h3>
+      <div id="imc-table">
+        <div className="table-header">
+          <h4>Imc</h4>
+          <h4>Classificação</h4>
+          <h4>Obesidade</h4>
+        </div>
+        {data.map((item) => (
+          <div className='table-data' key={item.info}>
+            <p>{item.classification}</p>
+            <p>{item.info}</p>
+            <p>{item.obesity}</p>
+          </div>
+        ))}
+      </div>
+      <Button id="back-btn" text="voltar" action={backToBegin} />
+    </div>
+  )
+}
+
+export default ImcTable
